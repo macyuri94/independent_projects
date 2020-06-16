c9649fb34b00135ee1349ef93af8ef9e8ec61905
macyuri94 <a17819@alunos.ipca.pt>
2020-06-16 04:16:51 +0100

    Organization of lines

--- simple_randomizer/app/src/main/java/com/example/simplerandomizer/MainActivity.kt
+++ simple_randomizer/app/src/main/java/com/example/simplerandomizer/MainActivity.kt
@@ -19,11 +19,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val rollButton = findViewById <Button> (R.id.rollButton)
-        val resultTextView = findViewById <TextView> (R.id.resultTextView)
-        val seekBar = findViewById <SeekBar> (R.id.seekBar)
-        val quantityTextView = findViewById <TextView> (R.id.quantityTextView)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        val quantityTextView = findViewById<TextView>(R.id.quantityTextView)
 
         rollButton.setOnClickListener {
             val random = Random().nextInt(seekBar.progress) + 1
